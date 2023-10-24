7d6c832c2815cad2b83793079e756b919cb81740
sungyj <sungyj@dankook.ac.kr>
2023-10-24 16:02:39 +0900

    Add LCD

---
+++ sungyj_working/LCD.c
@@ -0,0 +1,89 @@
+#include "LCD.h"
+#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()   {
+
+  if (wiringPiSetup () == -1) exit (1);
+
+  fd = wiringPiI2CSetup(I2C_ADDR);
+
+  lcd_init(); // setup LCD
+  
+  while (1)   {
+    printLCD("<Mode selection>", "Auto mode");
+    printLCD("<Mode selection>", "Manual mode");
+    printLCD("Warning!!", "Warning!!");
+  }
+
+  return 0;
+}
+
+
+
+void lcd_byte(int bits, int mode)   {
+
+  //Send byte to data pins
+  // bits = the data
+  // mode = 1 for data, 0 for command
+  int bits_high;
+  int bits_low;
+  // uses the two half byte writes to LCD
+  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT ;
+  bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT ;
+
+  // High bits
+  wiringPiI2CReadReg8(fd, bits_high);
+  lcd_toggle_enable(bits_high);
+
+  // Low bits
+  wiringPiI2CReadReg8(fd, bits_low);
+  lcd_toggle_enable(bits_low);
+}
+
+void lcd_toggle_enable(int bits)   {
+  // Toggle enable pin on LCD display
+  delayMicroseconds(500);
+  wiringPiI2CReadReg8(fd, (bits | ENABLE));
+  delayMicroseconds(500);
+  wiringPiI2CReadReg8(fd, (bits & ~ENABLE));
+  delayMicroseconds(500);
+}
+
+void lcd_init()   {
+  // Initialise display
+  lcd_byte(0x33, LCD_CMD); // Initialise
+  lcd_byte(0x32, LCD_CMD); // Initialise
+  lcd_byte(0x06, LCD_CMD); // Cursor move direction
+  lcd_byte(0x0C, LCD_CMD); // 0x0F On, Blink Off
+  lcd_byte(0x28, LCD_CMD); // Data length, number of lines, font size
+  lcd_byte(0x01, LCD_CMD); // Clear display
+  delayMicroseconds(500);
+}
+
+// clr lcd go home loc 0x80
+void ClrLcd(void)   {
+  lcd_byte(0x01, LCD_CMD);
+  lcd_byte(0x02, LCD_CMD);
+}
+
+// go to location on LCD
+void lcdLoc(int line)   {
+  lcd_byte(line, LCD_CMD);
+}
+
+// this allows use of any size string
+void typeln(const char *s)   {
+  while ( *s ) lcd_byte(*(s++), LCD_CHR);
+}
+
+void printLCD(const char *s_Line1, const char *s_Line2)  {
+    ClrLcd();
+    lcdLoc(LINE1);
+    typeln(s_Line1);
+    lcdLoc(LINE2);
+    typeln(s_Line2);
+    delay(500);
+}
